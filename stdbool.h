f23f7f759f4fd0aa79844cc964ff22199cfdac37
TinPlat <162581599+qv1145145@users.noreply.github.com>
2025-11-23 14:17:43 +0800

    Update stdbool.h

--- stdbool.h
+++ stdbool.h
@@ -1,10 +1,18 @@
-/* stdbool.h - Standard header for boolean type and values (C99 backport for C89) */
+/* stdbool.h - 最优化的布尔类型实现 */
 #ifndef _STDBOOL_H
 #define _STDBOOL_H
 
+/* 使用 unsigned char（节省内存）*/
 typedef unsigned char bool;
-#define true 1
-#define false 0
+#define true ((unsigned char)1)
+#define false ((unsigned char)0)
+
+/* C99 标准要求定义此宏 */
 #define __bool_true_false_are_defined 1
 
-#endif /* _STDBOOL_H 
+/* 调试时使用的字符串表示 */
+#ifdef DEBUG
+#define bool_str(b) ((b) ? "true" : "false")
+#endif
+
+#endif /* _STDBOOL_H */
